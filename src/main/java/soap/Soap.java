55100993ca4f349fd76e9b489826572ef49ed41f
cristianbg11 <20151256@ce.pucmm.edu.do>
2019-12-15 15:01:12 -0400

    probando heroku

--- src/main/java/soap/Soap.java
+++ src/main/java/soap/Soap.java
@@ -7,7 +7,7 @@ import javax.xml.ws.Endpoint;
 public class Soap {
     public static void init() throws Exception {
 
-        Endpoint.publish("http://localhost:80/ws/urls", new UrlWebServiceImpl());
+        Endpoint.publish("http://0.0.0.0/ws/urls", new UrlWebServiceImpl());
     }
 
 }
