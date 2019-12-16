9a184ccb4b83af36ad3443a9a8b8f54f4c2c16e5
cristianbg11 <20151256@ce.pucmm.edu.do>
2019-12-16 15:29:03 -0400

    heroku

--- src/main/java/api/Soap.java
+++ src/main/java/api/Soap.java
@@ -7,6 +7,6 @@ import javax.xml.ws.Endpoint;
 public class Soap {
     public static void init() {
 
-        Endpoint.publish("http://www.soapurl.me/ws/urls", new UrlWebServiceImpl());
+        Endpoint.publish("http://localhost:9090/ws/urls", new UrlWebServiceImpl());
     }
 }
